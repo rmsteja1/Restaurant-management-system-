b27799ff3efeda0635a2b5269cc3e2a9c150eab4
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-09-13 10:58:54 -0500

    Adding controller structure for menu

---
+++ controllers/menuController.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+//var menuCollection *mongo.Collection= database.OpenCollection(database.Client,"menu")
+
+func GetMenus() gin.HandlerFunc{
+	return func(c *gin.Context){
+		// ctx,cancel:=context.WithTimeout(context.Background(),100*time.Second)
+		// result,err:=menuCollection.Find(context.TODO(),bson.M{})
+		// defer cancel()
+		// if err!=nil{
+		// 	c.JSON(http.StatusInternalServerError,"error: error occured while listening to menu items")
+		// }
+		// var allMenus []bson.M
+		// if err=result.All(ctx,&allMenus);err!=nil{
+		// 	log.Fatal(err)
+		// }
+		// c.JSON(http.StatusOK,allMenus)
+
+		ctx,cancel:=context.WithTimeout(context.Background(),100*time.Second)
+		result,fetchError:=menuCollection.Find(context.TODO(),bson.M{})
+		defer cancel()
+		if fetchError!=nil{
+			c.JSON(http.StatusInternalServerError,"error:error occured while listening to the menu items")
+			return
+		}
+		var allMealItems []bson.M
+		if err:=result.All(ctx,&allMealItems); err!=nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK,allMealItems)
+	}
+}
+
+func GetMenu() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func CreateMenu() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func UpdateMenu() gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+}
